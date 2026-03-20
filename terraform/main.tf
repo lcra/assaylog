6aa8064b1cbfa8905aa53a026a85b7b42ed6c69a
lcra <60573667+lcra@users.noreply.github.com>
2026-03-21 00:24:33 +1030

    Migrate Terraform state to S3 backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,9 @@
 terraform {
+  backend "s3" {
+    bucket = "assaylog-terraform-state-548220614840"
+    key    = "terraform.tfstate"
+    region = "us-east-2"
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
